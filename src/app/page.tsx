1235f2392768bfcb548482fae4dd1077fca84d4b
Muktadirul <muktadirul.05@gmail.com>
2025-08-14 14:16:11 +0000

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,9 +17,9 @@ export default function Home() {
           <b>বিশেষ দ্রষ্টব্য: </b>
         </>)}
         <ul className="ps-3">
-          <li>{useText("I've used a context for storing the current language (default: Bangla)",'আমি এখানে React Context ব্যবহার করেছি এক্টিভ ল্যাংগুয়েজ স্টোর করার জন্য।)}</li>
-          <li>{useText("I've not used /en or /bn routes for simplicity",'ডেমো হওয়ায় আমি /en or /bn রাউট ব্যবহার করিনি।)}</li>
-          <li>{useText("Also, I've not used i18n for simplicity",'এবং, ডেমো হওয়ায় আমি i18n ব্যবহার করিনি।)}</li>
+          <li>{useText("I've used a context for storing the current language (default: Bangla)",'আমি এখানে React Context ব্যবহার করেছি এক্টিভ ল্যাংগুয়েজ স্টোর করার জন্য।')}</li>
+          <li>{useText("I've not used /en or /bn routes for simplicity",'ডেমো হওয়ায় আমি /en or /bn রাউট ব্যবহার করিনি।')}</li>
+          <li>{useText("Also, I've not used i18n for simplicity",'এবং, ডেমো হওয়ায় আমি i18n ব্যবহার করিনি।')}</li>
         </ul>
       </div>
     </div>
